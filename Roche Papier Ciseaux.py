2f8fd8f316144bcf22b670ccc949177583a53498
Sahon Shaha <108037485+SahonShaha@users.noreply.github.com>
2023-01-21 20:52:44 -0500

    Update Roche Papier Ciseaux.py

--- Roche Papier Ciseaux.py
+++ Roche Papier Ciseaux.py
@@ -1,41 +1,44 @@
+#Importe l'extension "random" qui nous permettra de choisir une action au hasard 
 import random
 
-
-def rock():
+#La fonction roche. Dépendemment sur l'option choisie par le joueur (la variable "userChoice"), il va déterminer s'il perd ou gagne. 
+def roche():
     if userChoice == "Roche":
-        print("It's a draw")
-    elif userChoice == "Scissors":
-        print("You lost")
+        print("Égalité :/")
+    elif userChoice == "Ciseaux":
+        print("Tu as perdu :(")
     elif userChoice == "Papier":
-        print("You win")
+        print("Tu as gagné :D")
 
-def scissors():
-    if userChoice == "Rock":
-        print("You won")
-    elif userChoice == "Scissors":
-        print("It's a draw")
-    elif userChoice == "Paper":
-        print("You lost")
+#La fonction ciseaux. Même role que la fonction roche. 
+def ciseaux():
+    if userChoice == "Roche":
+        print("Tu as gagné :D")")
+    elif userChoice == "Ciseaux":
+        print("Égalité :/")
+    elif userChoice == "Papier":
+        print("Tu as perdu :(")
 
-def paper():
-    if userChoice == "Rock":
-        print("You lost")
-    elif userChoice== "Scissors":
-        print("You won")
-    elif userChoice == "Paper":
-        print("It's a draw")
+#La fonction papier. Même role que la fonction roche et ciseaux. 
+def papier():
+    if userChoice == "Roche":
+        print("Tu as perdu :(")
+    elif userChoice== "Ciseaux":
+        print("Tu as gagné :D")
+    elif userChoice == "Papier":
+        print("Égalité :/")
 
 
-userChoice = input("Rock, Paper or Scissor? ")
+#Ici, l'ordinateur demande au joueur de choisir une option. 
+userChoice = input("Roche, Papier ou Ciseaux? ")
 
+#Ici, l'ordinateur choisit au hasard un numéro qui représentera roche, papier ou ciseaux
 res = random.randrange(1, 3)
 
+#Si le resultat du tirage est 1, l'ordinateur a choisi roche. 2, il a choisi papier. 3, il a choisi ciseaux. Ensuite, les fonctions seront activées comme montré au début. 
 if res == 1:
-    rock()
+    roche()
 elif res == 2:
-    paper()
+    papier()
 elif res == 3:
-    scissors()
-
-
-
+    ciseaux()
